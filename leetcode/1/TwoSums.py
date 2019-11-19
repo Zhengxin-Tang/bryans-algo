4262ee78b478a36e4eb8375f3cdceadcf22d9323
bryansOdyssey <609334666@qq.com>
2019-11-19 14:42:24 +1100

    Update and rename Two sums to TwoSums.py

---
+++ leetcode/1/TwoSums.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dic.keys():
+                return [dic[complement], i]
+            dic[nums[i]] = i
